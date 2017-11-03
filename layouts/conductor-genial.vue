5d3900fe7746f13e5023541284fabd331311dba9
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-11-03 17:16:55 -0600

    Create custom layout

---
+++ layouts/conductor-genial.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- START: Content -->
+  <section class="b-main-content">
+    <div class="container">
+      <AppHeader/>
+
+      <nuxt/>
+
+      <AppFooter/>
+    </div>
+  </section>
+</template>
+<script>
+  import AppHeader from '~/components/common/header.vue'
+  import AppFooter from '~/components/common/footer.vue'
+
+  export default {
+    components: {AppHeader, AppFooter}
+  }
+</script>
+<style>
+
+</style>
---
+++ pages/senales.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h1>Señales</h1>
+  </section>
+</template>
+<script>
+  export default {
+    layout: 'conductor-genial'
+  }
+</script>
