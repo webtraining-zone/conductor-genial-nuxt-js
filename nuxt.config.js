f2c356ef02250032b424355903e02c05dc918143
Enrique Diaz Romero <enrique.diaz@base22.com>
2017-11-13 10:07:23 -0600

    Add Axios to vendors array

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,29 +3,30 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'conductor-genial-nuxt-js',
+    title: 'Conductor Genial',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Un lugar donde aprender a ser un buen conductor' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'Un lugar donde aprender a ser un buen conductor'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Asap+Condensed|Open+Sans:400,700' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Asap+Condensed|Open+Sans:400,700'}
     ]
   },
   /*
   ** Customize SASS files
   */
-  css: [ 'bootswatch/cosmo/bootstrap.css', 'font-awesome/css/font-awesome.css', '@/assets/scss/main.scss' ],
+  css: ['bootswatch/cosmo/bootstrap.css', 'font-awesome/css/font-awesome.css', '@/assets/scss/main.scss'],
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: {color: '#3B8070'},
   /*
   ** Build configuration
   */
   build: {
+    vendor: ['axios'],
     /*
     ** Run ESLint on save
     */
